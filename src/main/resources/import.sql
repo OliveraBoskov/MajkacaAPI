88e5ef8f3d8a1a3a83d3162ad96e9874428e24bd
Olivera <boskov.olivera@gmail.com>
2018-04-10 19:53:28 +0200

    import sql restorani

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,15 @@ INSERT INTO hrana (id,description,name,price,typeF) VALUES (2,'obrok salata','Ce
 INSERT INTO hrana (id,description,name,price,typeF) VALUES (3,'toplo predjelo','potaz od bundeve', 270,'predjelo');
 
 INSERT INTO hrana (id,description,name,price,typeF) VALUES (4,'hladno predjelo','Karpaco od lososa', 1040,'predjelo');
+
+INSERT INTO korisnici (id, activation_link, active, email, password, role, ime) VALUES (1, 'vrjnfivwriuhi', true, 'boskov@gmail.com', 'olivera', 'admin', 'olivera');
+
+INSERT INTO korisnici (id, activation_link, active, email, password, role, ime) VALUES (2, 'vrjnhtbhtbghi', true, 'dovicin@gmail.com','nevena', 'moderator', 'nevena');
+
+INSERT INTO korisnici (id, activation_link, active, email, password, role, ime) VALUES (3, 'lkjhgfiuhi', true, 'zivolic@gmail.com','smiljana', 'korisnik', 'smiljana');
+
+INSERT INTO restoran (id, description, location, name, phone_number) VALUES (1, 'picerija', 'centar', 'Gondola', '021345189');
+
+INSERT INTO restoran (id, description, location, name, phone_number) VALUES (2, 'riblji restoran', 'Sremski Karlovci', 'Pasent', '021565732');
+
+INSERT INTO restoran (id, description, location, name, phone_number) VALUES (3, 'kineski restoran', 'Detelinara', 'Kineski Zid', '021333777');
